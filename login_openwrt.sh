0786dfe988cda74486e88536ceea4fd2c7229de8
PaperCrane <136219273+youngestdriver@users.noreply.github.com>
2026-05-17 21:38:41 +0800

    Update WANIP retrieval method in login_openwrt.sh
    Changed WANIP retrieval method from ifconfig to curl.

--- login_openwrt.sh
+++ login_openwrt.sh
@@ -7,7 +7,10 @@ PASSWORD=abcdefxxxxxx
 # 中国移动：cmcc 中国电信：telecom
 CHANNEL=cmcc
 
-WANIP=$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f2 | cut -d ' ' -f1)
+#WANIP=$(ifconfig eth0 | grep 'inet addr' | cut -d ':' -f2 | cut -d ' ' -f1)
+#从接口获取
+WANIP=`curl http://10.50.255.11/a79.htm 2>/dev/null | sed -n "s/^.*v46ip='\(.*\)'.*$/\1/p"`
+#从web获取
 
 LOGIN_URL="http://10.50.255.11:801/eportal/portal/login"
 LOGIN_REQUEST_URL="${LOGIN_URL}?callback=dr1003&login_method=1&user_account=%2C1%2C${USERID}%40${CHANNEL}&user_password=${PASSWORD}&wlan_user_ip=${WANIP}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=2&lang=zh-cn&v=8166&lang=zh"
